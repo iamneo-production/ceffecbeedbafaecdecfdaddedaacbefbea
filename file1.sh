a13f975b59f2752b08e73e80c7e163db9b151e24
Iamneo <techadmin@iamneo.ai>
2023-09-15 09:49:37 +0000

    Backup Commit

--- file1.sh
+++ file1.sh
@@ -1,9 +1,10 @@
 read -p "Enter day(DD): "day
 read -p "Enter month(MM): "month
 read -p "Enter year(YYYY): "year
-if[[ $day =~ ^[0-9]{2}$ && $month =~ ^[0-9]{2} && $year =~ ^[0-9]{4}$ ]];  then
-fordate="$day-$month-$year"
-echo "Date of Birth: $fordate"
+if[[ $day =~ ^[0-9]{2}$ && $month =~ ^[0-9]{2} && $year =~ ^[0-9]{4}$ ]]
+then 
+    fordate="$day-$month-$year"
+    echo "Date of Birth: $fordate"
 else
-echo "Invalid input,please enter in correct format."
+    echo "Invalid input,please enter in correct format."
 fi
