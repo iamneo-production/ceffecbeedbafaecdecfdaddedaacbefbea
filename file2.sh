d2957f3dc290388ac463848843eba097afc9c4f9
Iamneo <techadmin@iamneo.ai>
2023-09-15 10:08:03 +0000

    Backup Commit

--- file2.sh
+++ file2.sh
@@ -1,7 +1,8 @@
 read -p "Enter num1: "  num1
 read -p "Enter num2: "  num2
-total = $((num1+num2))
-if[ $total -gt 70 ]; then
+total = $num1 + $num2
+if[ $total -gt 70 ];
+then
 echo "Pass"
 else 
 echo "Fail"
